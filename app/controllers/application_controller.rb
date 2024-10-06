a0e2c223ffa03eee52d42f7c97dd148e662c7de3
Simon Baird <simon.baird@gmail.com>
2024-10-06 16:10:43 -0400

    Add protect_from_forgery to keep brakeman happy
    The out-of-the box GitHub workflows are neat.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
